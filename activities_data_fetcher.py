4aba447f9f16ebfee2f1c82200ffc8b731368ed1
Caroline Subramoney <csubram@umich.edu>
2020-05-11 17:34:15 -0400

    Renamed class name running_activities --> activities_data_fetcher

--- activities_data_fetcher.py
+++ activities_data_fetcher.py
@@ -3,12 +3,12 @@ from secrets import access_token
 import json
 
 
-class RunningActivities:
+class ActivitiesDataFetcher:
 
     def __init__(self):
         pass
 
-    def _get_next_page_of_50_activities(self, page_number):
+    def _get_next_page(self, page_number):
         query = 'https://www.strava.com/api/v3/activities'
 
         response = requests.get(
@@ -18,34 +18,34 @@ class RunningActivities:
                 'Authorization': 'Bearer {}'.format(access_token)
             },
             params={
-                'per_page': 50,
                 'page': page_number
             }
         )
 
         return response.json()
 
-    def get_all_activities(self):
-        page = 1
+    def get_all(self):
+        page_number = 1
         all_activities = []
 
         while True:
-            activities = self._get_next_page_of_50_activities(page)
+            activities = self._get_next_page(page_number)
 
             if not activities:
                 break
 
             all_activities.extend(activities)
-            page += 1
+            page_number += 1
 
         return all_activities
 
-    def get_all_activities_of_type(self, activity_type):
-        all_activities = self.get_all_activities()
-        running_activities = list(
+    def get_all_of_type(self, activity_type):
+        all_activities = self.get_all()
+        filtered_activities = list(
             filter(lambda x: (x['type'] == activity_type), all_activities))
+        return filtered_activities
 
 
 if __name__ == '__main__':
-    runs = RunningActivities()
-    runs.get_all_activities_of_type('Run')
+    fetcher = ActivitiesDataFetcher()
+    runs = fetcher.get_all_of_type('Run')
